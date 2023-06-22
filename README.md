f9fd6b69790efab1ac9e54d0b16ad28a87a9091d
Will Barnes <will.t.barnes@gmail.com>
2023-06-22 11:22:01 -0400

    Built site for gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# sphere-workshop-2023-talk
-Materials for my presentation on MOXSI hot plasma diagnostics at the 2023 SPHERE Workshop
--- pipeline/paths.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-
-root = pathlib.Path(__file__).parent.parent
-
-pipeline = root / 'pipeline'
-pipeline_data = pipeline / 'data'
-scripts = root / 'scripts'
